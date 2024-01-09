610213210a6de6e6c91e93ed1c5f76e45c0c6834
Thomas Zachary <thomaszachary06@proton.me>
2024-01-09 09:02:17 +0000

    Refactored RPN evaluation

--- src/RPN.hs
+++ src/RPN.hs
@@ -1,37 +1,37 @@
 module RPN
     ( evaluate
+    , BinaryOp(..)
     , Token(..)
     ) where
 
+data BinaryOp
+    = Add
+    | Sub
+    | Mul
+    | Pow
+
 data Token
     = Literal !Int
-    | Add
-    | Sub
-    | Mult
+    | BinaryOp !BinaryOp
 
 type Stack = [Int]
 
-add :: Stack -> Maybe Stack
-add (x:y:xs) = Just (x + y : xs)
-add _        = Nothing
-
-sub :: Stack -> Maybe Stack
-sub (x:y:xs) = Just (x + y : xs)
-sub _        = Nothing
-
-mult :: Stack -> Maybe Stack
-mult (x:y:xs) = Just (x * y : xs)
-mult _ = Nothing
+binaryop :: BinaryOp -> Stack -> Maybe Stack
+binaryop o (x:y:xs) = Just $ (:xs) 
+    $ case o of
+        Add -> y + x
+        Sub -> y - x
+        Mul -> y * x
+        Pow -> y ^ x
+binaryop _ _        = Nothing
 
 evaluate :: [Token] -> Maybe Int
 evaluate = (`f` [])
     where
-        f ::  [Token] -> Stack -> Maybe Int
-        f [] []       = Nothing
-        f [] (x:_)    = Just x
+        f :: [Token] -> Stack -> Maybe Int
+        f [] []     = Nothing
+        f [] (x:_)  = Just x
         f (t:ts) xs = f ts =<< case t of
-            Literal x -> Just $ x : xs
-            Add       -> add xs
-            Sub       -> sub xs
-            Mult      -> mult xs
+            Literal x  -> Just $ x : xs
+            BinaryOp o -> binaryop o xs
 
