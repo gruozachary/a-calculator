b4d56d0222f043f54df666cfb8567341de0d59e8
Thomas Zachary <thomaszachary06@proton.me>
2024-01-12 14:38:20 +0000

    Refactored Grapher module

--- src/Grapher.hs
+++ src/Grapher.hs
@@ -4,9 +4,10 @@ module Grapher
     , PlotEnvironment(..)
     , PlotState(..)
     ) where
-import SDL (Renderer, rendererDrawColor, V4 (V4), ($=), clear, present, pollEvents, Point(P), V2(V2), drawLine)
+import qualified SDL
 import RPN (Equation, evaluateWith)
-import Data.Map (fromList)
+import qualified Data.Map (fromList)
+import Data.Vector.Storable (fromList)
 import Foreign.C (CInt)
 import Control.Monad (void)
 import Control.Monad.Reader (ReaderT, asks, runReaderT, lift)
@@ -23,27 +24,21 @@ newtype PlotState = PlotState
     { scale :: (Double, Double)
     }
 
---type Plot = ReaderT PlotSettings IO
-
 type Plot = ReaderT PlotEnvironment (StateT PlotState IO)
-
-pairs :: [a] -> Maybe [(a, a)]
-pairs []        = Nothing
-pairs xs@(_:ys) = Just (zip xs ys)
 
 getCoordinate :: Equation -> Double -> Maybe (Double, Double)
 getCoordinate e x = (x,)
-    <$> evaluateWith e (fromList [("x", x)])
+    <$> evaluateWith e (Data.Map.fromList [("x", x)])
 
-processCoordinate :: (Double, Double) -> Plot (Point V2 CInt)
+processCoordinate :: (Double, Double) -> Plot (SDL.Point SDL.V2 CInt)
 processCoordinate (x, y) = do
     (sx, sy) <- lift $ gets scale
     (dx, dy) <- asks dimensions
-    return $ P $ V2
+    return $ SDL.P $ SDL.V2
         (round $ (fromIntegral dx / 2) + x * sx)
         (round $ (fromIntegral dy / 2) - y * sy)
 
-getPoints :: Plot (Maybe [Point V2 CInt])
+getPoints :: Plot (Maybe [SDL.Point SDL.V2 CInt])
 getPoints = do
     e  <- asks equation
     sx <- gets (fst . scale)
@@ -51,32 +46,33 @@ getPoints = do
         (mapM (getCoordinate e . (/ sx)) [-1000..1000])
 
 handleEvents :: Plot ()
-handleEvents = void pollEvents
+handleEvents = void SDL.pollEvents
 
-drawAxis :: Renderer -> Plot ()
+drawAxis :: SDL.Renderer -> Plot ()
 drawAxis r = do
     (x, y) <- asks $ uncurry ((,) `on` fromIntegral) . dimensions 
-    rendererDrawColor r $= V4 0 0 0 0
-    drawLine r (P $ V2 0 (y `div` 2)) (P $ V2 x (y `div` 2))
-    drawLine r (P $ V2 (x `div` 2) 0) (P $ V2 (x `div` 2) y)
+    SDL.rendererDrawColor r SDL.$= SDL.V4 0 0 0 0
+    SDL.drawLine r
+        (SDL.P $ SDL.V2 0 (y `div` 2))
+        (SDL.P $ SDL.V2 x (y `div` 2))
+    SDL.drawLine r
+        (SDL.P $ SDL.V2 (x `div` 2) 0)
+        (SDL.P $ SDL.V2 (x `div` 2) y)
 
-clearScreen :: Renderer -> Plot ()
+clearScreen :: SDL.Renderer -> Plot ()
 clearScreen r = do
-    rendererDrawColor r $= V4 255 255 255 255
-    clear r
+    SDL.rendererDrawColor r SDL.$= SDL.V4 255 255 255 255
+    SDL.clear r
 
-drawLines :: Renderer -> [Point V2 CInt] -> Plot ()
-drawLines r = mapM_ (mapM (uncurry $ drawLine r)) . pairs
-
-drawGraph' :: Renderer -> Plot ()
+drawGraph' :: SDL.Renderer -> Plot ()
 drawGraph' r = do
     handleEvents
     clearScreen r
     drawAxis r
-    rendererDrawColor r $= V4 255 0 0 255
-    getPoints >>= mapM_ (drawLines r)
-    present r
+    SDL.rendererDrawColor r SDL.$= SDL.V4 255 0 0 255
+    getPoints >>= mapM_ (SDL.drawLines r . fromList)
+    SDL.present r
     drawGraph' r
 
-drawGraph :: Renderer -> PlotEnvironment -> PlotState -> IO ()
-drawGraph r pe = evalStateT $ runReaderT (drawGraph' r) pe
+drawGraph :: SDL.Renderer -> PlotEnvironment -> PlotState -> IO ()
+drawGraph r = evalStateT . runReaderT (drawGraph' r)
